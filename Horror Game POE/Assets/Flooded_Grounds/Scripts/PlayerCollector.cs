bf95546685bb8a0ade072770e146aa1891ffa485
jade-hoppy <117459374+jade-hoppy@users.noreply.github.com>
2023-01-11 15:45:24 -0500

    Revert "revived monster again"
    This reverts commit 75f425ca861efd47eef1d8b067c0a6c022383d82.

--- Horror Game POE/Assets/Flooded_Grounds/Scripts/PlayerCollector.cs
+++ Horror Game POE/Assets/Flooded_Grounds/Scripts/PlayerCollector.cs
@@ -12,7 +12,7 @@ public class PlayerCollector : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-       scoreText.text = points.ToString() + " ";
+        scoreText.text = points.ToString() + " ";
     }
 
     // Update is called once per frame
